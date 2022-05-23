e82a6ef8962a54d8f5c80d4151ba0e72547652df
Robin <robin020813@163.com>
2022-05-24 00:26:14 +0800

    [milestone]完成基础任务，即将开始写输入检查部分

--- rm/main.cpp
+++ rm/main.cpp
@@ -1,16 +1,14 @@
 #include<iostream>
-#include<iomanip>
 #include <vector>
 #include <map>
 using namespace std;
 class Car{
 public:
     int blood;
-    int heat_17;
-    int heat_42;
+    int heat_max;
+    int heat;
     string team;
     string kind;
-    int number;
     int BloodCheck(string CarKind){
         if(CarKind=="B"){
             return 100;
@@ -21,28 +19,19 @@ public:
         }else if(CarKind=="G"){
             return 200;
         }
+        return -1;
     }
-    int Heat17Check(string CarKind){
+    int HeatCheck(string CarKind){
         if(CarKind=="B"){
             return 200;
         }else if(CarKind=="S"){
             return 300;
         }else if(CarKind=="Y"){
-            return -1;
-        }else if(CarKind=="G"){
-            return -1;
-        }
-    }
-    int Heat42Check(string CarKind){
-        if(CarKind=="B"){
-            return -1;
-        }else if(CarKind=="S"){
-            return -1;
-        }else if(CarKind=="Y"){
             return 300;
         }else if(CarKind=="G"){
-            return -1;
+            return 0;
         }
+      return -1;
     }
     string ChangeName(string name){
         if(name=="B"){
@@ -54,12 +43,12 @@ public:
         }else if(name=="G"){
             return "工程";
         }
+    return " ";
     }
 };
 class DiedReason{
 public:
     string reason;
-    int CarNum;
 };
 int main() {
     string CarTeam;
@@ -76,17 +65,15 @@ int main() {
         if (CarCommand == "A") {
             cin >> CarTeam >> CarKind >> CarNum;
             if (CarTeam == "R") {
-                RedCar[CarNum].number = CarNum;
                 RedCar[CarNum].kind = CarKind;
                 RedCar[CarNum].blood = RobotCar.BloodCheck(CarKind);
-                RedCar[CarNum].heat_17 = RobotCar.Heat17Check(CarKind);
-                RedCar[CarNum].heat_42 = RobotCar.Heat42Check(CarKind);
+                RedCar[CarNum].heat_max=RobotCar.HeatCheck(CarKind);
+                RedCar[CarNum].heat= 0;
             } else if (CarTeam == "B") {
-                BlueCar[CarNum].number = CarNum;
                 BlueCar[CarNum].kind = CarKind;
                 BlueCar[CarNum].blood = RobotCar.BloodCheck(CarKind);
-                BlueCar[CarNum].heat_17 = RobotCar.Heat17Check(CarKind);
-                BlueCar[CarNum].heat_42 = RobotCar.Heat42Check(CarKind);
+                BlueCar[CarNum].heat_max=RobotCar.HeatCheck(CarKind);
+                BlueCar[CarNum].heat= 0;
             }
         } else if (CarCommand == "F") {
             cin >> CarTeam >> CarNum >> CarHurt;
@@ -94,7 +81,6 @@ int main() {
                 RedCar[CarNum].blood -= CarHurt;
                 if (RedCar[CarNum].blood <= 0) {
                     RedDied[CarNum].reason = "blood";
-                    RedDied[CarNum].CarNum = CarNum;
                     RedCar.erase(CarNum);
                 }
             } else if (CarTeam == "B") {
@@ -105,34 +91,58 @@ int main() {
                 }
             }
             } else if (CarCommand == "H") {
-                
+            cin >> CarTeam >> CarNum >> CarHeat;
+            if (CarTeam == "R") {
+                if(RedCar[CarNum].kind=="Y")
+                    RedCar[CarNum].heat += CarHeat*30;
+                else
+                    RedCar[CarNum].heat += CarHeat*15;
+                if (RedCar[CarNum].heat >=RedCar[CarNum].heat_max) {
+                    RedDied[CarNum].reason = "heat";
+                    RedCar.erase(CarNum);
+                }
+            } else if (CarTeam == "B") {
+                if(BlueCar[CarNum].kind=="Y")
+                    BlueCar[CarNum].heat += CarHeat*30;
+                else
+                    BlueCar[CarNum].heat += CarHeat*15;
+                if (BlueCar[CarNum].heat >=BlueCar[CarNum].heat_max) {
+                    BlueDied[CarNum].reason = "heat";
+                    BlueCar.erase(CarNum);
+                }
+            }
             } else if (CarCommand == "END") {
                 map<int, Car>::iterator itRed, itBlue;
                 map<int, DiedReason>::iterator  RedDeath,BlueDeath;
                 cout << "红方存活：" << endl;
-                for (itRed = RedCar.begin(); itRed != RedCar.end(); itRed++) {
-                    if (itRed->second.kind == "Y")
-                        cout << "红方" << itRed->first << "号机器人   种类：" << RobotCar.ChangeName(itRed->second.kind) << "   血量："
-                             << itRed->second.blood << "   热量：" << itRed->second.heat_42 << endl;
-                    else
-                        cout << "红方" << itRed->first << "号机器人   种类：" << RobotCar.ChangeName(itRed->second.kind) << "   血量："
-                             << itRed->second.blood << "   热量：" << itRed->second.heat_17 << endl;
+                if(RedCar.size()==0){
+                    cout<<"没有兵种存活"<<endl;
+                }else {
+                    for (itRed = RedCar.begin(); itRed != RedCar.end(); itRed++) {
+                        cout << "红方" << itRed->first << "号机器人   种类：" << RobotCar.ChangeName(itRed->second.kind)
+                             << "   血量："
+                             << itRed->second.blood << "   热量：" << itRed->second.heat << "(热量上限为："
+                             << itRed->second.heat_max << ")" << endl;
+                    }
                 }
                     cout << "蓝方存活：" << endl;
-                for (itBlue = BlueCar.begin(); itBlue != BlueCar.end(); itBlue++) {
-                        if (itBlue->second.kind == "Y")
-                            cout << "蓝方" << itBlue->first << "号机器人   种类：" << RobotCar.ChangeName(itBlue->second.kind) << "   血量："
-                                 << itBlue->second.blood << "   热量：" << itBlue->second.heat_42 << endl;
-                        else
-                            cout << "蓝方" << itBlue->first << "号机器人   种类：" << RobotCar.ChangeName(itBlue->second.kind) << "   血量："
-                                 << itBlue->second.blood << "   热量：" << itBlue->second.heat_17 << endl;
+                if(BlueCar.size()==0){
+                     cout<<"没有兵种存活"<<endl;
+                }else {
+                    for (itBlue = BlueCar.begin(); itBlue != BlueCar.end(); itBlue++) {
+                        cout << "蓝方" << itBlue->first << "号机器人   种类：" << RobotCar.ChangeName(itBlue->second.kind)
+                             << "   血量："
+                             << itBlue->second.blood << "   热量：" << itBlue->second.heat << "(热量上限为："
+                             << itBlue->second.heat_max << ")" << endl;
+
+                    }
                 }
-                cout<<"伤亡情况："<<endl;
+                cout<<"阵亡情况："<<endl;
                 for(RedDeath = RedDied.begin();RedDeath != RedDied.end();RedDeath++){
-                    cout<<"红方"<<RedDeath->first<<"号机器人死亡     死因:"<<RedDeath->second.reason<<endl;
+                        cout<<"红方"<<RedDeath->first<<"号机器人死亡     死因:"<<RedDeath->second.reason<<endl;
                 }
-                for(RedDeath = RedDied.begin();RedDeath != RedDied.end();RedDeath++){
-                cout<<"蓝方"<<BlueDeath->first<<"号机器人死亡     死因:"<<RedDeath->second.reason<<endl;
+                for(BlueDeath = BlueDied.begin();BlueDeath != BlueDied.end();BlueDeath++){
+                        cout<<"蓝方"<<BlueDeath->first<<"号机器人死亡     死因:"<<BlueDeath->second.reason<<endl;
                 }
             return 0;
             }
