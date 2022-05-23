29b02c32fde64422280d4311310d2412e343e1a3
Robin <robin020813@163.com>
2022-05-23 17:39:44 +0800

    [feature]添加了A命令的功能，添加了初始化血量与热量函数

--- rm/main.cpp
+++ rm/main.cpp
@@ -9,23 +9,62 @@ using namespace std;
 class Car{
 public:
     int blood;
-    int heat;
+    int heat_17;
+    int heat_42;
     string team;
     string kind;
 
 };
-
+int BloodCheck(string CarKind){
+    if(CarKind=="infantry"){
+        return 100;
+    }else if(CarKind=="guard"){
+        return 200;
+    }else if(CarKind=="hero"){
+        return 200;
+    }else if(CarKind=="engineer"){
+        return 200;
+    }
+}
+int Heat17Check(string CarKind){
+    if(CarKind=="infantry"){
+        return 200;
+    }else if(CarKind=="guard"){
+        return 300;
+    }else if(CarKind=="hero"){
+        return -1;
+    }else if(CarKind=="engineer"){
+        return -1;
+    }
+}
+int Heat42Check(string CarKind){
+    if(CarKind=="infantry"){
+        return -1;
+    }else if(CarKind=="guard"){
+        return -1;
+    }else if(CarKind=="hero"){
+        return 300;
+    }else if(CarKind=="engineer"){
+        return -1;
+    }
+}
 int main() {
     string CarTeam;
     string CarKind;
-    int Carnum;
+    int CarNum;
     int CarHurt;
     int CarHeat;
     string CarCommand;
+    map<int,Car> RedCar,BlueCar;
     while (1) {
-        cin >> CarCommand;
         if(CarCommand=="A"){
-
+            cin >>CarTeam>>CarKind>>CarNum;
+            if(CarTeam=="R"){
+                RedCar[CarNum].kind=CarKind;
+                RedCar[CarNum].blood=BloodCheck(CarKind);
+                RedCar[CarNum].heat_17=Heat17Check(CarKind);
+                RedCar[CarNum].heat_42= Heat42Check(CarKind);
+            }
         }else if(CarCommand=="F"){
 
         }else if(CarCommand=="H"){
